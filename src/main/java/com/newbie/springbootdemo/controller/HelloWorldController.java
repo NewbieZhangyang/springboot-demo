880e2ebf3966181d5908e8acb77ba4d840cfda42
Newbie Zhangyang <yy810896908@163.com>
2019-05-23 18:35:16 +0800

    1、增加springboot热部署依赖：spring-boot-devtools 2、在application.properties中，修改tomcat端口号：server.port=8081 3、新建两个类：ConfigHome、ConfigNewbie。测试以下功能：Spring在创建bean实例时，从配置文件中获取参数值，赋值给bean的属性。

--- src/main/java/com/newbie/springbootdemo/controller/HelloWorldController.java
+++ src/main/java/com/newbie/springbootdemo/controller/HelloWorldController.java
@@ -1,15 +1,32 @@
 package com.newbie.springbootdemo.controller;
 
+import com.newbie.springbootdemo.domain.ConfigHome;
+import com.newbie.springbootdemo.domain.ConfigNewbie;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
+    @Value("${com.newbie.name}")
+    private String name;
+    @Value("${com.newbie.hobby}")
+    private String hobby;
+
+    @Autowired
+    private ConfigNewbie configNewbie;
+
+    @Autowired
+    private ConfigHome configHome;
 
     @RequestMapping("helloWorld")
     public String helloWorld(){
         System.out.println("HelloWorld Controller");
-        return "Hello World.This is my first SpringBoot Project."  ;
+        String message = "name :"+this.name+" , hobby:"+this.hobby+" >>> ";
+        message += "configNewbie.name : "+this.configNewbie.getName() + " , configNewbie.hoby : "+this.configNewbie.getHobby()+" >>> ";
+        message += "home.address : "+configHome.getAddress()+" , home.type: "+configHome.getType()+",randomId : "+configHome.getRandomId()+" >>>";
+        return  message;
     }
 
 
