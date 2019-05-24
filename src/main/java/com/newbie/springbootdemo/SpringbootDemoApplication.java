1de261c86ec06a3f17343371db2f705efc20dbd2
Newbie Zhangyang <yy810896908@163.com>
2019-05-24 16:36:20 +0800

    1、在打包项目时，取消对spring-boot-starter-tomcat插件的依赖，以便将项目部署到外部的tomcat中进行发布

--- src/main/java/com/newbie/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/newbie/springbootdemo/SpringbootDemoApplication.java
@@ -2,11 +2,28 @@ package com.newbie.springbootdemo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class SpringbootDemoApplication {
+public class SpringbootDemoApplication extends SpringBootServletInitializer {
 
+    /**
+     * 修改启动类，在外部的tomcat中部署时使用
+     * 参考资源：https://www.cnblogs.com/qianzf/p/8405479.html
+     * @param builder
+     * @return
+     */
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+        return builder.sources(SpringbootDemoApplication.class);
+    }
+
+    /**
+     * 使用内部的tomcat时，默认的启动类
+     * @param args
+     */
     public static void main(String[] args) {
         SpringApplication.run(SpringbootDemoApplication.class, args);
     }
