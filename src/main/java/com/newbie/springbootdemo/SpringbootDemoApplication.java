e307cf9d72fe84db324f7708c36bf090dee87815
Newbie Zhangyang <yy810896908@163.com>
2019-05-24 11:53:30 +0800

    1、将项目回退到开始位置； 2、修改tomcat的端口号为8081 3、增加Springboot热部署支持

--- src/main/java/com/newbie/springbootdemo/SpringbootDemoApplication.java
+++ src/main/java/com/newbie/springbootdemo/SpringbootDemoApplication.java
@@ -1,13 +1,10 @@
 package com.newbie.springbootdemo;
 
-import com.newbie.springbootdemo.domain.ConfigHome;
-import com.newbie.springbootdemo.domain.ConfigNewbie;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties({ConfigNewbie.class, ConfigHome.class})
 public class SpringbootDemoApplication {
 
     public static void main(String[] args) {
--- src/main/java/com/newbie/springbootdemo/controller/HelloWorldController.java
+++ src/main/java/com/newbie/springbootdemo/controller/HelloWorldController.java
@@ -1,7 +1,5 @@
 package com.newbie.springbootdemo.controller;
 
-import com.newbie.springbootdemo.domain.ConfigHome;
-import com.newbie.springbootdemo.domain.ConfigNewbie;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -9,26 +7,13 @@ import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class HelloWorldController {
-    @Value("${com.newbie.name}")
-    private String name;
-    @Value("${com.newbie.hobby}")
-    private String hobby;
-
-    @Autowired
-    private ConfigNewbie configNewbie;
-
-    @Autowired
-    private ConfigHome configHome;
 
     @RequestMapping("helloWorld")
-    public String helloWorld(){
-        System.out.println("HelloWorld Controller");
-        String message = "name :"+this.name+" , hobby:"+this.hobby+" >>> ";
-        message += "configNewbie.name : "+this.configNewbie.getName() + " , configNewbie.hoby : "+this.configNewbie.getHobby()+" >>> ";
-        message += "home.address : "+configHome.getAddress()+" , home.type: "+configHome.getType()+",randomId : "+configHome.getRandomId()+" >>>";
-        return  message;
+    public String helloWorld() {
+        String message = "Hello World Controller";
+        System.out.println(message);
+        return message;
     }
-
 
 
 }
